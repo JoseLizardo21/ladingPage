b831bad1bec4119d4eca20de61827e534e43e1cf
Jose <jose.lizardo21.cont@gmail.com>
2022-11-17 16:32:23 -0500

    se agregó una ventana emergente

--- src/public/scripts/code.js
+++ src/public/scripts/code.js
@@ -3,11 +3,17 @@ function inicio(){
     const img2 = document.getElementById("cel2");
     const img3 = document.getElementById("cel3");
     const img4 = document.getElementById("cel4");
+    let btnx = document.getElementById("btnXMessage");
+    btnx.addEventListener('click', cerrar);
     setTimeout(()=>{
         img1.style.marginTop = "250px";
         img2.style.marginLeft = "400px";
         img3.style.marginLeft = "100px";
         img4.style.marginTop = "-100px";
-    }, 500)
+    }, 500);
+}
+function cerrar(){
+    const ventMensaje = document.getElementById('cont-ZMessage');
+    ventMensaje.style.visibility = 'hidden'
 }
 window.addEventListener("load", inicio);
